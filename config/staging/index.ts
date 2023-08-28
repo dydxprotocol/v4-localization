3308852fc9d652bd2a0278df72eee8086bfd1b8d
jaredvu <jaredvu@gmail.com>
2023-08-28 10:03:01 -0700

    v0.0.11: Expose env endpoints

---
+++ config/staging/index.ts
@@ -0,0 +1,7 @@
+import devEndpoints from './dev_endpoints.json';
+import endpoints from './endpoints.json';
+import walletsV2Staging from './walletsV2.json';
+
+export const DEV_ENDPOINTS = devEndpoints;
+export const ENDPOINTS_STAGING = endpoints;
+export const WALLETS_V2_STAGING = walletsV2Staging;
