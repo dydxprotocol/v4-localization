b5093fc6d6d3669c6aaa23a3e22b4823eeba6b09
John Huang <johnqh@yahoo.com>
2023-10-23 12:09:38 -0700

    TRCL-2794 Removed all config files for iOS (#115)

--- config/prod/index.ts
+++
@@ -1,5 +0,0 @@
-import endpoints from './endpoints.json';
-import walletsV2Production from './walletsV2.json';
-
-export const ENDPOINTS = endpoints;
-export const WALLETS_V2_PRODUCTION = walletsV2Production;
