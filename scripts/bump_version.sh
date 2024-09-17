2ec5b280312cf33cfc7a3ac70aad7a69c95f235a
Mike <mike-dydx@users.noreply.github.com>
2024-09-17 11:17:38 -0400

    add new script for PRs

--- scripts/bump_version.sh
+++ scripts/bump_version.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
 
-# search for the first line that starts with "version" in package.json
-# get the value in the quotes
+# Parse the boolean parameter
+SHOW_IMPORTANT_TEXT=true
+if [ "$1" = "--no-warning" ]; then
+  SHOW_IMPORTANT_TEXT=false
+fi
+
+# Search for the first line that starts with "version" in package.json
+# Get the value in the quotes
 VERSION=$(cat package.json | jq -r '.version')
 
 echo "Current version is $VERSION. Enter new version (or press enter to skip):"
 read NEW_VERSION
 
-#if NEW_VERSION is not empty, replace the version in package.json
+# If NEW_VERSION is not empty, replace the version in package.json
 if [ -n "$NEW_VERSION" ]; then
   sed -i '' "s/  \"version\": \"$VERSION\"/  \"version\": \"$NEW_VERSION\"/" package.json
   echo "Version bumped to $NEW_VERSION"
   npm i
 fi
 
-npm run generate
+# ANSI color code for red
+RED='\033[0;31m'
+# ANSI color code for resetting color
+NC='\033[0m'
+
+# Display the important text only if SHOW_IMPORTANT_TEXT is true
+if [ "$SHOW_IMPORTANT_TEXT" = true ]; then
+  echo ""
+  echo "${RED}**************************************************************"
+  echo "* IMPORTANT: If you are creating a PR, run \`npm run prepare_for_pr\` *"
+  echo "**************************************************************${NC}"
+  echo ""
+fi
